0aae483a9021af1159ace7c848c936a7334a8212
cowboyliao <2730647052@qq.com>
2022-12-31 23:33:18 +0800

    alpha

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "FiraCode Nerdfont Mono:pixelsize=20:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -93,37 +93,39 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 
+/* bg opacity */
+float alpha = 0.8;
+
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+    /* 8 normal colors */
+    "#e9e9ed",
+    "#f52a65",
+    "#587539",
+    "#8c6c3e",
+    "#2e7de9",
+    "#9854f1",
+    "#007197",
+    "#6172b0",
 
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+    /* 8 bright colors */
+    "#a1a6c5",
+    "#f52a65",
+    "#587539",
+    "#8c6c3e",
+    "#2e7de9",
+    "#9854f1",
+    "#007197",
+    "#3760bf",
 
-	[255] = 0,
+    [255] = 0,
 
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"gray90", /* default foreground colour */
-	"black", /* default background colour */
+    /* more colors can be added after 255 to use with DefaultXX */
+    "#3760bf",     
+    "#99a7df",
+    "#3760bf", /* default foreground colour */
+    "#e1e2e7", /* default background colour */
 };
-
 
 /*
  * Default colors (colorname index)
@@ -133,7 +135,6 @@ unsigned int defaultfg = 258;
 unsigned int defaultbg = 259;
 unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
-
 /*
  * Default shape of cursor
  * 2: Block ("█")
