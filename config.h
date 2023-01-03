638faadcac9e2752b3954c3b1c6c76f8d71065d1
cowboyliao <2730647052@qq.com>
2023-01-03 17:21:49 +0800

    23/1/3

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FiraCode Nerdfont Mono:pixelsize=20:antialias=true:autohint=true";
+static char *font = "FiraCode Nerdfont Mono:pixelsize=15:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -94,7 +94,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.85;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
